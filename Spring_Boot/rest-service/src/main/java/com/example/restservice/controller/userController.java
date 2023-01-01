4e15c6a11b7b65fe3a982135d11ce28fcccd6659
James Huang <jameshuang@Jamess-MacBook-Pro.local>
2022-12-31 23:20:14 -0500

    add controller logic

--- Spring_Boot/rest-service/src/main/java/com/example/restservice/controller/userController.java
+++ Spring_Boot/rest-service/src/main/java/com/example/restservice/controller/userController.java
@@ -1,10 +1,37 @@
 package com.example.restservice.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.restservice.model.User;
+import com.example.restservice.service.userService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/user")
+
 public class userController {
+
+    @Autowired
+    private userService userService;
+
+    @PostMapping("/add")
+    public User addUser(@RequestBody User user) {
+        //TODO: Need implementation
+        return null;
+    }
+
+    @GetMapping("/getAll")
+    public List<User> getAllUser() {
+        //TODO: Need implementation
+        return null;
+    }
+
+    @GetMapping("/getByName")
+    public User getUserByName(@RequestParam("name") String name) {
+        //TODO: Need implementation
+        return null;
+    }
+
 }
