fe0d4f0d19aace760aeeb9d214e80c0480a71e1c
James Huang <jameshuang@Jamess-MacBook-Pro.local>
2023-01-02 15:15:01 -0500

    add master repo

---
+++ Spring_Boot_Dev/rest-service/src/main/java/com/example/restservice/dao/userDao.java
@@ -0,0 +1,13 @@
+package com.example.restservice.dao;
+
+import com.example.restservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface userDao extends JpaRepository<User, Integer>{
+
+    int addUser(User user);
+}
---
+++ Spring_Boot_Dev/rest-service/src/main/java/com/example/restservice/model/User.java
@@ -0,0 +1,36 @@
+package com.example.restservice.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "user_t")
+public class User {
+    /*
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    
+     */
+    private UUID id;
+    private String email;
+    private String password;
+    private String name;
+    private String sex;
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '}';
+    }
+
+}
